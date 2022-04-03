12c2da315270794377278f793a7fb635a2247b6c
Liz Graham <rileygraham739@gmail.com>
2022-04-03 01:39:41 +0100

    make max unmald
    Took 1 minute

--- src/main/java/net/lizistired/starminerrecrafted/mixin/MixinDebugScreenOverlay.java
+++ src/main/java/net/lizistired/starminerrecrafted/mixin/MixinDebugScreenOverlay.java
@@ -3,45 +3,22 @@ package net.lizistired.starminerrecrafted.mixin;
 
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.lang.management.BufferPoolMXBean;
-import java.lang.management.ManagementFactory;
 import java.util.List;
-import java.util.Objects;
 
 import static net.lizistired.starminerrecrafted.basics.blockentities.GravityCoreBlockEntity.relativeDirection;
 
 @Mixin(DebugHud.class)
 public abstract class MixinDebugScreenOverlay {
-    @Unique
-    private static final List<BufferPoolMXBean> pools = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);
 
-    @Unique
-    private static final BufferPoolMXBean directPool;
-
-    static {
-        BufferPoolMXBean found = null;
-
-        for (BufferPoolMXBean pool : pools) {
-            if (pool.getName().equals("direct")) {
-                found = pool;
-                break;
-            }
-        }
-
-        directPool = Objects.requireNonNull(found);
-    }
     @Inject(method = "getRightText", at = @At("RETURN"))
-    private void appendShaderPackText(CallbackInfoReturnable<List<String>> cir) {
+    private void appendDebugDirectionText(CallbackInfoReturnable<List<String>> cir) {
         List<String> messages = cir.getReturnValue();
-
         messages.add("");
         messages.add("Direction: " + relativeDirection.toString().toUpperCase());
-        //messages.add("Direction: " + relativeDirection.toString().toUpperCase());
         messages.add("");
     }
 }
